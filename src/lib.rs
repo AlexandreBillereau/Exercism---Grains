bc6e834bdc0ccac348d596bde9058ab5e7809c74
alxeandre <alexandre.mr.billerau@gmail.com>
2023-03-27 13:45:40 -0400

    finish

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {s}");
+    match s {
+     s if s <= 64 && s != 0 =>  1*2u64.pow(s-1),
+     _           =>  panic!("Square must be between 1 and 64")
+    }
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    let mut result: u64 = 0;
+    for i in 1..=64 {
+        result += square(i);
+    }
+    result
 }
